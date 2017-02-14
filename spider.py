5211e60df96edaa01499da3ba4cd7741eacf2ca5
Li Xinyang <lixinyang1026@gmail.com>
2017-02-14 15:46:46 +0800

    feat: Fetching course list and course detailed list

--- spider.py
+++ spider.py
@@ -1,52 +1,130 @@
-from bs4 import BeautifulSoup
-from config import ACCOUNT
+from bs4      import BeautifulSoup
+from config   import ACCOUNT
 from selenium import webdriver
-from urllib2 import urlopen, URLError, HTTPError
+from urllib2   import urlopen, URLError, HTTPError
+
+import httplib
 import cookielib
 import json
 import mechanize
 import os
 import string
 import time
+import os.path
 
-# Browser setup
-cookiejar = cookielib.CookieJar()
-browser = mechanize.Browser()
-browser.set_cookiejar(cookiejar)
+# Constants
+DATA_COURSE_LIST = './DATA_COURSE_LIST.json'
+DATA_COURSE_DETAILED_LIST = './DATA_COURSE_DETAILED_LIST.json'
 
-# Selenium browser setup
-chrome = webdriver.Chrome()
+URL_COURSE_LIST = 'https://frontendmasters.com/courses/'
 
+# Global Browser Setup
+browser = webdriver.Firefox()
 
-def login(username, password, browser=browser):
+def browser_login():
     BASE_URL = 'https://frontendmasters.com/login/'
-    browser.open(BASE_URL)
+    browser.get(BASE_URL)
+    browser.implicitly_wait(2) # waiting request to complete (depends on your connection)
 
-    # Select the first form
-    browser.select_form(nr=0)
-    browser.form['rcp_user_login'] = username
-    browser.form['rcp_user_pass'] = password
-    browser.submit()
+    username = browser.find_element_by_id('rcp_user_login')
+    username.send_keys(ACCOUNT['username'])
+    password = browser.find_element_by_id('rcp_user_pass')
+    password.send_keys(ACCOUNT['password'])
 
-    return browser
+    browser.implicitly_wait(2) # waiting request to complete
+    browser.find_element_by_id('rcp_login_submit').click()
+    browser.implicitly_wait(5)
 
 
-def get_course_list(browser=browser):
-    bs_course_page = BeautifulSoup(browser.response().read(), "html.parser")
-    course_titles = bs_course_page.find_all('h2')
+# Func: Retrieve courses list
+def _get_course_list():
+    BASE_URL = URL_COURSE_LIST
+    browser.get(BASE_URL)
+    bs_course_page = BeautifulSoup(browser.page_source, "html.parser")
+    course_titles = bs_course_page.select('h2.title')
     course_links = []
 
     for title in course_titles:
         link = title.find('a')
 
         if link is not None:
-            course = {'title': link.getText(), 'url': link['href']}
-
+            title = link.getText()
+            course = {'title': title, 'url': link['href']}
             course_links.append(course)
 
     return course_links
 
+def save_course_list():
+    with open(DATA_COURSE_LIST, 'w') as file:
+        file.write(json.dumps(_get_course_list()))
 
+def retrive_course_list():
+    with open(DATA_COURSE_LIST, 'r') as file:
+        return json.load(file)
+
+
+# Func: Retrieve detailed section list for each course
+def _get_section_data(sections_items):
+    sections = []
+    for item in sections_items:
+        # Course section data structure
+        course_section = {
+            'title': None,
+            'subsections': []
+        }
+
+        course_section['title'] = item.find(
+            'h4', {'class': 'video-nav-section-title'}
+        ).getText()
+
+        videos_section = item.find('ul')
+        videos_section_items = videos_section.find_all('li')
+
+        videos_data = get_videos_data(videos_section_items)
+        course_section['subsections'].extend(videos_data)
+
+        sections.append(course_section)
+
+    return sections
+
+def _get_detailed_course_list(course_list):
+    detailed_course_list = []
+
+    for course in course_list:
+        # Course detail data structure
+        course_detial = {
+            'title': course['title'],
+            'url': course['url'],
+            'sections': []
+        }
+
+        browser.get(course_detial['url'])
+        browser.implicitly_wait(2)
+        soup_page = BeautifulSoup(browser.page_source, 'html.parser')
+
+        # Find video nav list
+        sections = soup_page.find('ul', {'class': 'video-nav-list'})
+        sections_items = sections.find_all(
+            'li', {'class': 'video-nav-section'}
+        )
+
+        sections = _get_section_data(sections_items)
+        course_detial['sections'].extend(sections)
+
+        detailed_course_list.append(course_detial)
+
+    return detailed_course_list
+
+def save_course_detailed_list(course_list):
+    with open(DATA_COURSE_DETAILED_LIST, 'w') as file:
+        file.write(json.dumps(_get_detailed_course_list(course_list)))
+
+def retrive_course_detailed_list():
+    with open(DATA_COURSE_DETAILED_LIST, 'r') as file:
+        return json.load(file)
+
+
+# Func: Retrieve video CDN
 def get_videos_data(videos_section_items):
     subsections = []
 
@@ -69,66 +147,15 @@ def get_videos_data(videos_section_items):
 
     return subsections
 
-
-def get_section_data(sections_items):
-    sections = []
-
-    for item in sections_items:
-        # Course section data structure
-        course_section = {'title': None, 'subsections': []}
-
-        course_section['title'] = item.find(
-            'h4', {'class': 'video-nav-section-title'}
-        ).getText()
-
-        videos_section = item.find('ul')
-        videos_section_items = videos_section.find_all('li')
-
-        videos_data = get_videos_data(videos_section_items)
-        course_section['subsections'].extend(videos_data)
-
-        sections.append(course_section)
-
-    return sections
-
-
-def get_detailed_course_list(course_list, browser=browser):
-    detailed_course_list = []
-
-    for course in course_list:
-        # Course detail data structure
-        course_detial = {'title': None, 'url': None, 'sections': []}
-
-        course_detial['url'] = course['url']
-        course_detial['title'] = course['title']
-
-        browser.open(course_detial['url'])
-        soup_page = BeautifulSoup(browser.response().read(), 'html.parser')
-
-        # Find video nav list
-        sections = soup_page.find('ul', {'class': 'video-nav-list'})
-        sections_items = sections.find_all(
-            'li', {'class': 'video-nav-section'}
-        )
-
-        sections = get_section_data(sections_items)
-        course_detial['sections'].extend(sections)
-
-        detailed_course_list.append(course_detial)
-
-    return detailed_course_list
-
-
 def download_file(url, path):
 
     if not os.path.isfile(path):
-        print url
+        print(url)
         buff = urlopen(url)
-        print "Downloading: %s" % (path)
+        print("Downloading: %s" % (path))
 
         with open(path, 'wb') as local_file:
             local_file.write(buff.read())
-
 
 def format_filename(s):
     """Take a string and return a valid filename constructed from the string.
@@ -146,7 +173,6 @@ an invalid filename.
     filename = filename.replace(' ', '_')  # I don't like spaces in filenames.
     return filename
 
-
 def save_data():
     # Browser with all login info.
     browser = login(ACCOUNT['username'], ACCOUNT['password'])
@@ -156,27 +182,11 @@ def save_data():
         detailed_course_list = get_detailed_course_list(course_list)
         file.write(json.dumps(detailed_course_list))
 
-
 def load_data(path):
     with open(path, 'r') as file:
         return json.loads(file.read())
 
-
-def real_browser_login(chrome=chrome):
-    URL_LOGIN = 'https://frontendmasters.com/login/'
-    chrome.get(URL_LOGIN)
-    time.sleep(2)
-
-    username = chrome.find_element_by_id('rcp_user_login')
-    username.send_keys(ACCOUNT['username'])
-    password = chrome.find_element_by_id('rcp_user_pass')
-    password.send_keys(ACCOUNT['password'])
-
-    time.sleep(2)
-    chrome.find_element_by_id('rcp_login_submit').click()
-
-
-def get_video_source(video_link, browser=chrome):
+def get_video_source(video_link, browser=browser):
     browser.get(video_link)
     time.sleep(1)
     source_link = browser.find_element_by_tag_name(
@@ -185,12 +195,10 @@ def get_video_source(video_link, browser=chrome):
     return source_link
 
 
-courses_data = load_data('./DATA_DOWNLOADABLE.json')
-
+# Func: Download resources via CDN
 def write_downloadable_data(courses_data):
     with open('DATA_DOWNLOADABLE.json', 'w') as file:
         file.write(json.dumps(courses_data))
-
 
 def get_downloadable_links(courses_data):
     for course in courses_data:
@@ -200,28 +208,21 @@ def get_downloadable_links(courses_data):
 
                 if subsection['downloadable_url'] is None:
                     video_url = url + subsection['url']
-                    print "Retriving: {0}/{1}/{2}".format(
+                    print("Retriving: {0}/{1}/{2}".format(
                         format_filename(course['title']),
                         format_filename(section['title']),
-                        format_filename(subsection['title']))
+                        format_filename(subsection['title'])))
                     url_str = get_video_source(video_url)
-                    print "Video URL: {0}".format(url_str)
+                    print("Video URL: {0}".format(url_str))
                     subsection['downloadable_url'] = url_str
                     write_downloadable_data(courses_data)
                     time.sleep(3)
 
     return courses_data
 
-
-# real_browser_login()
-# get_downloadable_links(courses_data)
-
-courses_detailed_data = load_data('./DATA_DOWNLOADABLE.json')
-
 def create_path(path):
     if not os.path.exists(path):
         os.makedirs(path)
-
 
 def download_courses(courses_array):
     # Create download directory
@@ -238,8 +239,8 @@ def download_courses(courses_array):
 
             for i2, subsection in enumerate(section['subsections']):
                 subsection_title = subsection['title']
-                print "Downloading: {0}".format(
-                    format_filename(subsection_title))
+                print("Downloading: {0}".format(
+                    format_filename(subsection_title)))
 
                 filename = str(i1) + '-' + str(i2) + format_filename(
                     section_title) + '|' + format_filename(
@@ -250,4 +251,18 @@ def download_courses(courses_array):
                 download_file(subsection['downloadable_url'], file_path)
 
 
-download_courses(courses_detailed_data)
+
+# APP: Spider Logic
+browser_login()
+
+course_list = []
+if not os.path.isfile(DATA_COURSE_LIST):
+    save_course_list()
+course_list = retrive_course_list()
+
+time.sleep(15)
+
+course_detailed_list = []
+if not os.path.isfile(DATA_COURSE_DETAILED_LIST):
+    save_course_detailed_list(course_list)
+course_detailed_list = retrive_course_detailed_list()
